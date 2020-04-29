03c4408a032df13a4070ff9ce373c411bd810167
cyboryan <cyboryan@gmail.com>
2020-04-29 19:09:25 +0800

    Deleted unnecessary js

--- assets/js/admin.checkbox.js
+++
@@ -1,5 +0,0 @@
-var checkBoxes = $('tbody .user_checkbox');
-    checkBoxes.change(function () {
-        $('#addAdmin').prop('disabled', checkBoxes.filter(':checked').length < 1);
-    });
-    $('tbody .user_checkbox').change();
--- assets/js/enroll.checkbox.js
+++
@@ -1,5 +0,0 @@
-var checkBoxes = $('tbody .student_checkbox');
-    checkBoxes.change(function () {
-        $('#enrollStudentSubmit').prop('disabled', checkBoxes.filter(':checked').length < 1);
-    });
-    $('tbody .student_checkbox').change();
