9dc66d3d9fcf59910960f78c781a32b3a43ac158
cyboryan <cyboryan@gmail.com>
2020-05-14 21:03:51 +0800

    Move channel list to a new dedicated file
    This change was made for organization since channel list consists of a lot of stuff and unfortunately can't be minimized on an IDE. This change should alleviate a bit for the development.

--- assets/js/index.js
+++ assets/js/index.js
@@ -156,53 +156,6 @@ $(document).ready(function()
         else $('body').removeClass('sb-sidenav-toggled');
     });
 
-
-    /**
-     * Here are the Channel IDs of the YouTubers. With this,
-     * we can load their channel info easily with the help
-     * of the YouTube Data API V3
-     */
-    var channelId = new Array(
-        "UC37BgAsDXAuoKhQEzGbDemw", // cyboryan
-        "UCkeY7_-SaiLOzgCPIISpseQ", // liza ann mae guinocor
-        "UC4lOgaL2xSTrWxpFrX6uSZA", // mikone calungsod
-        "UCtDrV-JMhipwfqnOqudwB8Q", // Jezrel David Lumakin
-        "UCPfbILihlOQI7r-YEf1rWnw", // Georbel Felisario
-        "UC_pCT17MGZagqWWdg0qkUtw", // renz argie oronos
-        "UC9TjwyJ07j4qDJfQ5VjN9lg", // vince clifford
-        "UCoIj09XmE9jxfaiG91V1vmQ", // jacob lopez wenceslao
-        "UC-Ik4qSR6MOvZbBBqgm83fw", // czejan rae
-        "UClF7kC4UkmH7G62uT3qbz9A", // rodjy vlogs
-        "UC_gnvZEZMgXxvlzwfdxk1Cg", // precious gorre
-        "UCS_aEOKESHVf_AB7CM03Biw", // clark quinn cabardo
-        "UC6s5oVqvHvFMNikuasQeKGA", // yonexei
-        "UCwOzGcNajublZXQO1JxccSg", // m1z
-        "UClAmKlOklMnzzpreV1p-w6Q", // arron coraza
-        "UCQV8eYDKiQi3sqOHBIUZN_Q", // lovelytisado
-        "UCacjUTvggkVRtP7HtyJKKHQ", // mickthestick
-        "UCpPzT23jO2OfMtv_s2r7-9g", // kimmy arcena
-        "UCqV8AH_HC3oYzQh9NWw1l0A", // lorainne centino
-        "UCqO3xF_0to0U66igiPcv--w", // danisse tabudlong
-        "UCyIYwb3IoPli42HwmU3bvUQ", // nathaniel ricci vlogs
-        "UCJi63Su4-C0P9w02aJk3wYQ", // juda minaling
-        "UC9xAYD6yy7GGZwqzWHjoAzA", // antonette cords
-        "UCDN8IQDcszWSxeiCqBnaF1Q", // hannah maxine
-        "UCNo9pOnhzr32xF8xMC0ypHg", // maryjul abapo
-        "UCVnbyXfgHiHMTXP9LRBcp5g", // anne telin
-        "UC_-hGvRSdSHz-qqKNPy-n2g", // mark baybay vlogger
-        "UCv_P5Zruu8_Qm6cTYZvZggw", // bobby d music
-        "UC4VSPIA94al9vCVUTr6vSSw", // jaylou
-        "UCR6q6y56SiNoB-0xzOJlrbg", // angel lie
-        "UCl8xo6AKqbhPrfMFDGTtHaw", // shawn valenzona
-        "UCOAVIUbLYCq84pdMHZZkLuw", // princess aicelle
-        "UC_yDEeZQ119wW-ig92uo7rA", // jenelyn digman
-        "UCnZPE5lIXRun3ca6PED-8eg", // mark film tv
-        "UCz4TtrIdeVnNis9hQTsjY9w", // Ann Marie Augustine
-        "UCF2OEYeORTtCFTfmeY6GyTA", // Goods YT - gefford neil roa godoy
-        "UCzJdoWhBz_pDQW5n-qEtLQQ", // Ron Ny
-        "UCIWcVeoHS35gu9ytCEi73jg" // Baybay Pakatok
-    );
-
     /**
      * Based on the list of channels on an array, requests one-by-one the channel data and
      * show them to the table.
